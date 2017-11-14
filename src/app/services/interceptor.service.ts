e257e2907c2386d3e3d77625c0d742d43360c859
iain plimmer <iainplimmer@MacBook-Pro-de-iain.local>
2017-11-14 16:17:44 +0000

    We need to inject the TokenService to avoid cyclical dependecies.

--- src/app/services/interceptor.service.ts
+++ src/app/services/interceptor.service.ts
@@ -1,19 +1,21 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Injector } from '@angular/core';
 import { HttpRequest, HttpHandler, HttpEvent, HttpInterceptor } from '@angular/common/http';
 import { Observable } from 'rxjs/Observable';
 import { TokenService } from './../services/token.service';
 
-
-
 @Injectable()
 export class InterceptorService implements HttpInterceptor {
 
-  constructor(private tokenService : TokenService) { }
+  constructor(private injector: Injector) { }
 
   intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
+
+    //  We need to inject the TokenService to avoid cyclical dependecies.
+    const tokenService = this.injector.get(TokenService);
+
     request = request.clone({
       setHeaders: {
-        Authorization: `Bearer ${this.tokenService.AccessToken}`
+        Authorization: `Bearer ${tokenService.AccessToken}`
       }
     });
     return next.handle(request);
