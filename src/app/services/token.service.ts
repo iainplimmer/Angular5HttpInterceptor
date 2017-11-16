79a2f3a1b96bf4b2773facb4c2ddaf88b02cb617
iain plimmer <iainplimmer@iains-iMac.local>
2017-11-16 08:00:35 +0000

    TokenService now uses the HttpClient.

--- src/app/services/token.service.ts
+++ src/app/services/token.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { Http } from '@angular/http';
+import  { HttpClient } from '@angular/common/http';
 import { Constants } from './../constants/constants';
 
 @Injectable()
@@ -8,29 +8,23 @@ export class TokenService {
   private authData$: string = `grant_type=password&username=${Constants.Username}&password=${Constants.Password}&ApplicationOrigin=1`;
   private accessToken: string = '';
 
+  //  Getter to return the currenly set access token
   get AccessToken(): string {
+    if (this.accessToken === '') {
+      this.accessToken = localStorage.getItem('token');
+    }
     return this.accessToken;
   }
 
-  constructor(private http : Http) { 
-    //  Let's first try to get the token from localstorage, and if it exists set this as a property
-    //  on the service.
-    let lsToken = localStorage.getItem('token');
-    if (lsToken) {
-      this.accessToken = lsToken;
-    }
-    else {
-      this.CreateNewToken();
-    }
-  }
+  constructor(private httpClient : HttpClient) { }
 
   //  Sets the token to the localstorage to be shared throughout the application
   CreateNewToken(){
-    this.http.post(Constants.TokenUrl, this.authData$).toPromise()
-    .then((d) => {
-      this.accessToken = d.json().access_token;
-      localStorage.setItem('token', this.accessToken);
-    });
+    this.httpClient.post(Constants.TokenUrl, this.authData$)
+      .subscribe((d) => {
+        this.accessToken = d['access_token'];
+        localStorage.setItem('token', this.accessToken);
+      });
   }
 
 }
